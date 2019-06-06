bcfe921a04eec84e646af03b6cd321c78fbecb07
uenokohei <k.cola119@ezweb.ne.jp>
2019-06-06 11:14:55 +0900

    ローカルで実行

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,10 +15,3 @@ app.get('/users', (req, res) => {
 });
 
 export const api = functions.https.onRequest(app);
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
