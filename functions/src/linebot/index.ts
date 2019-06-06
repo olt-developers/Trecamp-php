70cd409d8958b1fc4c88d2d99768220f930ff106
kouhei ueno <k.cola119@gmail.com>
2019-06-06 21:03:35 +0900

    連携done

--- functions/src/linebot/index.ts
+++ functions/src/linebot/index.ts
@@ -1,6 +1,7 @@
 import * as line from '@line/bot-sdk';
 import { WebhookEvent } from '@line/bot-sdk';
 import * as express from 'express';
+import * as request from 'request';
 import { lineToken } from './config';
 
 /**
@@ -19,14 +20,7 @@ export const router = express.Router();
 const client = new line.Client(lineToken as line.ClientConfig);
 
 router.post('/', (req, res) => {
-  const signature = req.get('x-line-signature');
-  if (!signature) {
-    throw new line.SignatureValidationFailed('no signature');
-  }
-
-  if (!line.validateSignature(JSON.stringify(req.body), lineToken.channelSecret, signature)) {
-    throw new line.SignatureValidationFailed('signature validation failed', signature);
-  }
+  middleware(req);
   Promise.all(req.body.events.map(handleEvent))
     .then(result => res.json(result))
     .catch(e => {
@@ -35,18 +29,87 @@ router.post('/', (req, res) => {
     });
 });
 
-const handleEvent = async (event: WebhookEvent): Promise<any> => {
-  console.log(JSON.stringify(event));
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
+const middleware = (req: express.Request) => {
+  const signature = req.get('x-line-signature');
+  if (!signature) {
+    throw new line.SignatureValidationFailed('no signature');
   }
+  if (!line.validateSignature(JSON.stringify(req.body), lineToken.channelSecret, signature)) {
+    throw new line.SignatureValidationFailed('signature validation failed', signature);
+  }
+};
+
+const handleEvent = async (event: WebhookEvent): Promise<any> => {
   if (event.source.userId === undefined) {
     return Promise.resolve(null);
   }
   const profile = await client.getProfile(event.source.userId);
-  const echo: line.TextMessage = {
-    text: `${profile.displayName}さん、${event.message.text}`,
-    type: 'text',
+
+  if (event.type === 'accountLink') {
+    if (event.link.result === 'ok') {
+      client.replyMessage(event.replyToken, {
+        text: `@${profile.displayName}さん、連携成功！`,
+        type: 'text',
+      });
+    } else {
+      client.replyMessage(event.replyToken, {
+        text: `@${profile.displayName}さん、連携失敗...`,
+        type: 'text',
+      });
+    }
+  }
+
+  if (event.type === 'message' && event.message.type === 'text') {
+    if (event.message.text === '連携') {
+      return postAccountLink(profile);
+    }
+
+    const echo: line.TextMessage = {
+      text: `${profile.displayName}さん、${event.message.text}`,
+      type: 'text',
+    };
+    return client.replyMessage(event.replyToken, echo);
+  }
+
+  return Promise.resolve(null);
+};
+
+const postAccountLink = (profile: line.Profile) => {
+  const option = {
+    headers: {
+      Authorization: `Bearer ${lineToken.channelAccessToken}`,
+    },
+    uri: `https://api.line.me/v2/bot/user/${profile.userId}/linkToken`,
   };
-  return client.replyMessage(event.replyToken, echo);
+  request.post(option, (err, res, body) => {
+    const linkToken = JSON.parse(body).linkToken;
+    const options = {
+      headers: {
+        Authorization: `Bearer ${lineToken.channelAccessToken}`,
+        'Content-Type': 'application/json',
+      },
+      json: {
+        messages: [
+          {
+            altText: 'Account Link',
+            template: {
+              actions: [
+                {
+                  label: 'ユーザー連携',
+                  type: 'uri',
+                  uri: `https://trecamp-55883.firebaseapp.com/login?linkToken=${linkToken}`,
+                },
+              ],
+              text: 'Trecampとユーザー連携しますか？',
+              type: 'buttons',
+            },
+            type: 'template',
+          },
+        ],
+        to: profile.userId,
+      },
+      uri: 'https://api.line.me/v2/bot/message/push',
+    };
+    request.post(options);
+  });
 };
