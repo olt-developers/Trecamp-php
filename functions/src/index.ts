61b63b6997e470943ec034d6cf52fd2e86b7f881
uenokohei <k.cola119@ezweb.ne.jp>
2019-06-07 17:33:32 +0900

    firestore接続

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,24 @@
 import * as express from 'express';
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import { router as linebot } from './linebot';
+
+admin.initializeApp({
+  credential: admin.credential.cert(require('../config/serviceAccountKey.json')),
+  databaseURL: 'https://trecamp-server.firebaseio.com',
+});
+
 const app = express();
 
-// https://developers.line.biz/console/channel/1573235259/basic/
+const db = admin.firestore();
+
 app.use('/callback', linebot);
 
-app.get('/users', (req, res) => {
-  const users = [
-    { id: 1, name: 'イリヤ' },
-    { id: 2, name: '美遊' },
-    { id: 3, name: 'クロエ' },
-    { id: 4, name: 'オルタ' },
-    { id: 5, name: 'マシュ' },
-  ];
-  res.send(JSON.stringify(users));
+app.get('/users', async (req, res, next) => {
+  const snapshot = await db.collection('users').get();
+  const users = snapshot.docs.map(doc => doc.data());
+  console.log(users);
+  res.json(users);
 });
 
 export const api = functions.https.onRequest(app);
