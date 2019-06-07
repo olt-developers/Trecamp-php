5bb6b7aeed9535f9e67a535db10977db2d396f05
uenokohei <k.cola119@ezweb.ne.jp>
2019-06-07 19:44:09 +0900

    login機構

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,17 +2,20 @@ import * as express from 'express';
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { router as linebot } from './linebot';
+import { router as auth } from './auth';
 
 admin.initializeApp({
   credential: admin.credential.cert(require('../config/serviceAccountKey.json')),
   databaseURL: 'https://trecamp-server.firebaseio.com',
 });
 
-const app = express();
-
 const db = admin.firestore();
 
+const app = express();
+
 app.use('/callback', linebot);
+
+app.use('/login', auth);
 
 app.get('/users', async (req, res, next) => {
   const snapshot = await db.collection('users').get();
