6db8dc13ed17896e17cc71762b471c021f1cd112
kouhei ueno <k.cola119@gmail.com>
2019-06-24 20:27:00 +0900

    traininngs endpoint

--- functions/src/routes/trainings.ts
+++ functions/src/routes/trainings.ts
@@ -1,15 +1,31 @@
 import * as express from 'express';
-import { db } from '../firestore';
+import { trainingsRef, typesRef } from '../firestore';
+import { FieldValue } from '@google-cloud/firestore';
 
 export const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  const snapshot = await db.collection('trainings').get();
+  const snapshot = await trainingsRef.get();
   const trainings = snapshot.docs.map(doc => doc.data());
   res.json(trainings);
 });
 
 router.post('/', async (req, res, next) => {
-  console.log(req.body.name);
-  console.log(req.body);
+  const typeIndex = req.body.type;
+  const snapshot = await typesRef.doc(`${typeIndex}`).get();
+  const type = snapshot.data();
+  if (!type) {
+    return res.status(500).end();
+  }
+  const training = {
+    uid: req.body.uid,
+    date: req.body.date,
+    type: typeIndex,
+    value: req.body.value,
+    score: req.body.value * type.point,
+    comment: req.body.comment || '',
+    createdAt: FieldValue.serverTimestamp(),
+  };
+  trainingsRef.add(training);
+  res.json(training);
 });
